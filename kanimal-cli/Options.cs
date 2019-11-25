75b468ba789b0ac66b5c138d2baa8b06970500cc
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2019-11-24 21:54:15 -0500

    Cleanup

--- kanimal-cli/Options.cs
+++ kanimal-cli/Options.cs
@@ -3,7 +3,7 @@ using CommandLine;
 
 namespace kanimal_cli
 {
-    internal abstract class ProgramOptions
+    public abstract class ProgramOptions
     {
         [Option('v', "verbose", Required = false, HelpText = "Enable debug output.")]
         public bool Verbose { get; set; }
@@ -15,21 +15,21 @@ namespace kanimal_cli
         public string OutputPath { get; set; } = "output";
     }
 
-    internal abstract class ConversionOptions : ProgramOptions
+    public abstract class ConversionOptions : ProgramOptions
     {
         [Option('S', "strict", Required = false, HelpText = "When writing to scml, enabling this flag ")]
         public bool Strict { get; set; }
     }
 
     [Verb("dump", HelpText = "Output a dump of the specified kanim.")]
-    internal class DumpOptions : ProgramOptions
+    public class DumpOptions : ProgramOptions
     {
         [Value(0)] public IEnumerable<string> Files { get; set; }
     }
 
     // For ones with Output and Input specifiers
     [Verb("convert", HelpText = "Convert between formats.")]
-    internal class GenericOptions : ConversionOptions
+    public class GenericOptions : ConversionOptions
     {
         [Option('I', "input-format", Required = true, HelpText = "The input format, from [kanim, scml]")]
         public string InputFormat { get; set; }
@@ -41,19 +41,19 @@ namespace kanimal_cli
     }
 
     [Verb("scml", HelpText = "Convert kanim to scml. Convenience verb equivalent to 'convert -I kanim -O scml'.")]
-    internal class KanimToScmlOptions : ConversionOptions
+    public class KanimToScmlOptions : ConversionOptions
     {
         [Value(0)] public IEnumerable<string> Files { get; set; }
     }
 
     [Verb("kanim", HelpText = "Convert scml to kanim. Convenience verb equivalent to 'convert -I scml -O kanim'.")]
-    internal class ScmlToKanimOptions : ConversionOptions
+    public class ScmlToKanimOptions : ConversionOptions
     {
         [Value(0)] public string ScmlFile { get; set; }
     }
 
     [Verb("batch-convert", HelpText = "Given an Assets/ directory, attempt to batch convert kanim to scml.")]
-    internal class BatchConvertOptions : ConversionOptions
+    public class BatchConvertOptions : ConversionOptions
     {
         [Value(0)] public string AssetDirectory { get; set; }
     }
